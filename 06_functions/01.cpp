de2ab5d5c81f16592da17d6a12ff825a97738519
ernesto-official02 <ernestanmol@gmail.com>
2024-06-11 15:23:17 +0530

    hi

--- 06_functions/01.cpp
+++ 06_functions/01.cpp
@@ -2,12 +2,21 @@
 using namespace std;
 
 
-void my_func()
-{
-    cout<<"My function"<<endl;
+void fact(int num)
+{   int sum =1;
+    for(int i =1 ; i<=num ;i++)
+    {
+    
+        sum*=i;
+
+    }
+    cout<<sum<<endl;
 }
 int main()
 {
-    my_func();
+    int num;
+    cout<<"ENter the you want to get the factorial"<<endl;
+    cin>>num;
+    fact(num);
 
 }
