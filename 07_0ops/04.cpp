a0bce09e586e48143be9c8d6914d836c3ccc06c0
ernesto-official02 <ernestanmol@gmail.com>
2024-06-12 15:36:41 +0530

    hi

---
+++ 07_0ops/04.cpp
@@ -0,0 +1,20 @@
+//constructor concept in cpp
+#include<iostream>
+using namespace std;
+
+
+class employee
+{
+    public:
+    employee()
+    {
+        cout<<"Constructor is called"<<endl;
+    }
+
+};
+
+int main()
+{
+    employee emp;
+
+}
