8410428ef92f70dd38b3e0a286ed717c8e255fe9
ernesto-official02 <ernestanmol@gmail.com>
2024-06-12 14:01:06 +0530

    hi

--- 06_functions/03.cpp
+++ 06_functions/03.cpp
@@ -1,10 +1,12 @@
 //pass by value concepts using c++
 #include<iostream>
 using namespace std;
+
 void test(int n)
 {  n++;
     cout<<"From test : "<<n<<endl;
 }
+
 int main()
 {   int n ;
     cin>>n;
