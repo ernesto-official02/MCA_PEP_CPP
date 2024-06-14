e70e6da07f00073ce506c900e9572d07be2adf63
ernesto-official02 <ernestanmol@gmail.com>
2024-06-14 13:17:46 +0530

    hi

--- 04_pointers/01.cpp
+++ 04_pointers/01.cpp
@@ -8,21 +8,21 @@ int main()
 {
     int var =5;
     cout<<"here's the address or variable named as var"<<endl;
-    cout<<&var<<endl;
+    cout<<&var<<endl;//address
     int *get= &var;
     //pointer refrencing 
-    cout<<get<<endl;
+    cout<<get<<endl;//address
     //pointer dereferencing  // getting or accesing the value of stored value of address
-    cout<<*get<<endl;
+    cout<<*get<<endl;//value
     //address of the pointer
-    cout<<&get<<endl;
+    cout<<&get<<endl;//address of ptr
 
 
     //double pointer
 
     int **ptr2=&get;
     cout<<"Address of  pointer using another double pointer "<<endl;
-    cout<<ptr2<<endl;
+    cout<<ptr2<<endl;//get address
     //deferencing the double pointer
     cout<<**ptr2<<endl;
 }
